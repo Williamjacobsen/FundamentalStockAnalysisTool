492755a05de53000fc1aec6e244fec364a5d9431
Williamjacobsen <villi05.v.j@gmail.com>
2024-05-26 10:53:58 +0200

    backend & figi

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send("Success!");
+});
+
+app.get("/figi", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.openfigi.com/v2/mapping",
+      [{ idType: "TICKER", idValue: "AAPL" }],
+      {
+        headers: {
+          "Content-Type": "application/json",
+          //"X-OPENFIGI-APIKEY": "YOUR_API_KEY", // no api key, max 25 requests per minute
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(error.response ? error.response.status : 500)
+      .send(error.response ? error.response.data : "Error fetching data");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
--- frontend/src/pages/Stock.jsx
+++ frontend/src/pages/Stock.jsx
@@ -1,7 +1,15 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const StockPage = (props) => {
   const { ticker } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {};
+
+    fetchData();
+  }, [ticker]);
 
   return <div>{ticker}</div>;
 };
