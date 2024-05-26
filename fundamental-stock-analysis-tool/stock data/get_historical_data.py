9dc3362f4f76883b03a4c4f6eb90b114164937fc
Williamjacobsen <villi05.v.j@gmail.com>
2024-05-26 10:23:26 +0200

    removed files

--- fundamental-stock-analysis-tool/stock data/get_historical_data.py
+++
@@ -1,7 +0,0 @@
-# https://roic.ai/api/company/financial-table?financialPeriod=annual&currentCurrency=FRC&category=incomeStatement&figiGlobal=BBG001S5N8V8&figi=BBG000B9XRY4
-
-
-
-# https://stockanalysis.com/stocks/msft/financials/
-
-
--- fundamental-stock-analysis-tool/stock data/main.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import yahooquery as yfq
-
-#stock = yfq.Ticker("aapl")
-#print(stock.get_financial_data())
-#print(stock.earnings)
-
-#print(getattr(yf.Ticker('aapl'), "financials"))
-
